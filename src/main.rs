cfe47a4878be917f94453d350793d20a5c203f9d
Jean Fiedler <JFI@kd.local>
2019-11-08 13:58:32 +0100

    write ini file with more than one value

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,33 @@ use winreg::enums::{HKEY_LOCAL_MACHINE, KEY_READ};
 use std::fs::File;
 use std::io::prelude::*;
 
-fn regreadvalue (regpath: &str, regvalue: &str, mut _inifile: std::fs::File) {
+fn regreadvalue (regpath: &str, regvalue: &str, mut inifile: &std::fs::File) {
     let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
 
     let subkey = hklm.open_subkey_with_flags(regpath, KEY_READ)
                     .expect("Failed to open subkey");
     let thevalue: String = subkey.get_value(regvalue)
                     .expect("Failed to read product name");
+
+    let iniline: String = format!("{}\\{}={}\n", regpath, regvalue, thevalue);
+    let binaryiniline = iniline.as_bytes();
+
+    inifile.write_all(binaryiniline).expect("could not write line");
     
-    println!("{}\\{}={}", regpath, regvalue, thevalue);
-    _inifile.write_all(b"yeah\n");
 }
 
 
 fn main() -> std::io::Result<()> {
 
-    
-    //regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "CurrentVersion");
-    //regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "EditionID");
-    //regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ReleaseId");
-    //regreadvalue(r#"SOFTWARE\Microsoft\Cryptography"#, "MachineGuid");
-
-    let mut inifile = File::create("foo.txt")?;
-    inifile.write_all(b"[main]\n")?;
-    inifile.write_all(b"[data]\n")?;
-    
-    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ProductName", inifile);
+    let mut inifile = File::create("output.ini")?;
+    inifile.write_all(b"[FileInfo]\n")?;
+    inifile.write_all(b"Version=1\n")?;
+    inifile.write_all(b"[Machine]\n")?;
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ProductName", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "CurrentVersion", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "EditionID", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ReleaseId", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Cryptography"#, "MachineGuid", &inifile);
     
     Ok(())
 }
