3c3a96fa7f2a82c3d0321afc0616cbc836be1944
Jean Fiedler <JFI@kd.local>
2020-01-14 15:35:39 +0100

    starting refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 extern crate winreg;
 
-use winreg::enums::{HKEY_LOCAL_MACHINE, KEY_READ};
+use winreg::enums::{RegType, HKEY_LOCAL_MACHINE, KEY_READ};
+use winreg::types::FromRegValue;
 use std::fs::File;
 use std::io::prelude::*;
 
+fn display_reg_value(rv: &winreg::RegValue) -> String {
+    match rv.vtype {
+        RegType::REG_SZ | RegType::REG_EXPAND_SZ | RegType::REG_MULTI_SZ => {
+            String::from_reg_value(rv).unwrap_or_default()
+        }
+        RegType::REG_DWORD => u32::from_reg_value(rv).unwrap_or_default().to_string(),
+        RegType::REG_QWORD => u64::from_reg_value(rv).unwrap_or_default().to_string(),
+        _ => panic!("can only process reg value of type string, u32 or u64"),
+    }
+}
+
+#[allow(dead_code)]
 fn regreadvalue_string (regpath: &str, regvalue: &str, mut inifile: &std::fs::File) {
     let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
 
@@ -23,6 +36,8 @@ fn regreadvalue_string (regpath: &str, regvalue: &str, mut inifile: &std::fs::Fi
             },
         };
     }
+
+#[allow(dead_code)]
 fn regreadvalue_int (regpath: &str, regvalue: &str, mut inifile: &std::fs::File) {
         let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
     
@@ -42,6 +57,8 @@ fn regreadvalue_int (regpath: &str, regvalue: &str, mut inifile: &std::fs::File)
                 },
             };
         }
+    
+        #[allow(dead_code)]
     fn regreadvalue_intbig (regpath: &str, regvalue: &str, mut inifile: &std::fs::File) {
             let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
         
@@ -62,6 +79,7 @@ fn regreadvalue_int (regpath: &str, regvalue: &str, mut inifile: &std::fs::File)
                 };
             }
 
+            #[allow(dead_code)]
     fn regreadvalue_gen (regpath: &str, regvalue: &str, mut inifile: &std::fs::File) {
         let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
     
@@ -103,25 +121,42 @@ fn regreadvalue_int (regpath: &str, regvalue: &str, mut inifile: &std::fs::File)
             };
         }
 
+fn regreadvalue(regpath: &str, regvalue: &str, mut inifile: &std::fs::File) {
+    let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
+    let subkey = hklm.open_subkey_with_flags(regpath, KEY_READ);
+    let thevalue = match subkey {
+        Ok(subkey) => {
+            let v = subkey.get_raw_value(regvalue).unwrap();
+            display_reg_value(&v)
+        }
+        Err(_) => "".to_string(),
+    };
+    let iniline = format!("{}\\{}={}\n", regpath, regvalue, thevalue);
+    let binaryiniline = iniline.as_bytes();
+    inifile
+        .write_all(binaryiniline)
+        .expect("could not write line");
+}
+
 fn main() -> std::io::Result<()> {
 
     let mut inifile = File::create("output.ini")?;
     inifile.write_all(b"[FileInfo]\n")?;
     inifile.write_all(b"Version=1\n")?;
     inifile.write_all(b"[Machine]\n")?;
-    regreadvalue_gen(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ProductName", &inifile);
-    regreadvalue_gen(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "CurrentVersion", &inifile);
-    regreadvalue_gen(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "EditionID", &inifile);
-    regreadvalue_gen(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ReleaseId", &inifile);
-    regreadvalue_gen(r#"SOFTWARE\Microsoft\Cryptography"#, "MachineGuid", &inifile);
-    regreadvalue_gen(r#"SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName"#, "ComputerName", &inifile);
-    regreadvalue_gen(r#"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"#, "Domain", &inifile);
-    regreadvalue_gen(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "ComputerHardwareId", &inifile);
-    regreadvalue_gen(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "SystemManufacturer", &inifile);
-    regreadvalue_gen(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "SystemProductName", &inifile);
-    regreadvalue_gen(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "BIOSVersion", &inifile);
-    regreadvalue_gen(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "BIOSReleaseDate", &inifile);
-    regreadvalue_gen(r#"SYSTEM\HardwareConfig\Current"#, "EnclosureType", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ProductName", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "CurrentVersion", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "EditionID", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Windows NT\CurrentVersion"#, "ReleaseId", &inifile);
+    regreadvalue(r#"SOFTWARE\Microsoft\Cryptography"#, "MachineGuid", &inifile);
+    regreadvalue(r#"SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName"#, "ComputerName", &inifile);
+    regreadvalue(r#"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"#, "Domain", &inifile);
+    regreadvalue(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "ComputerHardwareId", &inifile);
+    regreadvalue(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "SystemManufacturer", &inifile);
+    regreadvalue(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "SystemProductName", &inifile);
+    regreadvalue(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "BIOSVersion", &inifile);
+    regreadvalue(r#"SYSTEM\ControlSet001\Control\SystemInformation"#, "BIOSReleaseDate", &inifile);
+    regreadvalue(r#"SYSTEM\HardwareConfig\Current"#, "EnclosureType", &inifile);
 
     Ok(())
 }
