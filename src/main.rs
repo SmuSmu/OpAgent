2a1419f49610cfbc4034217ce4a8f33294818541
Jean Fiedler <JFI@kd.local>
2020-01-14 16:00:47 +0100

    remove panic

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ fn display_reg_value(rv: &winreg::RegValue) -> String {
     use winreg::enums::RegType::*;
     use winreg::types::FromRegValue;
     match rv.vtype {
-        REG_SZ | REG_EXPAND_SZ | REG_MULTI_SZ => {
-            String::from_reg_value(rv).unwrap_or_default()
-        }
-        REG_DWORD => u32::from_reg_value(rv).unwrap_or_default().to_string(),
-        REG_QWORD => u64::from_reg_value(rv).unwrap_or_default().to_string(),
-        _ => panic!("can only process reg value of type string, u32 or u64"),
+        REG_NONE                            => "REG_NONE".to_string(),
+        REG_SZ                              => String::from_reg_value(rv).unwrap_or_default(),
+        REG_EXPAND_SZ                       => String::from_reg_value(rv).unwrap_or_default(),
+        REG_MULTI_SZ                        => "REG_MULTI_SZ".to_string(),
+        REG_DWORD                           => u32::from_reg_value(rv).unwrap_or_default().to_string(),
+        REG_QWORD                           => u64::from_reg_value(rv).unwrap_or_default().to_string(),
+        REG_BINARY                          => "REG_BINARY".to_string(),
+        REG_DWORD_BIG_ENDIAN                => "REG_DWORD_BIG_ENDIAN".to_string(),
+        REG_LINK                            => "REG_LINK".to_string(),
+        REG_RESOURCE_LIST                   => "REG_RESOURCE_LIST".to_string(),
+        REG_FULL_RESOURCE_DESCRIPTOR        => "REG_FULL_RESOURCE_DESCRIPTOR".to_string(),
+        REG_RESOURCE_REQUIREMENTS_LIST      => "REG_RESOURCE_REQUIREMENTS_LIST".to_string(),
     }
 }
 
