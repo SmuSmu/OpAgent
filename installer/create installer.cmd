900727ec55c1799769dcc7d4032c9d20749a4c9a
Jean Fiedler <JFI@kd.local>
2020-07-06 17:14:26 +0200

    SCHTASKS

--- installer/create installer.cmd
+++ installer/create installer.cmd
@@ -1,4 +1,4 @@
 candle installer.wxs
-light installer.wixobj -dcl:high -ext WixUIExtension
+light installer.wixobj -dcl:high -ext WixUIExtension.dll -ext WixUtilExtension.dll
 del installer.wixobj
 del installer.wixpdb
