aba57f3494f730165ab5cb4d504061f492e3491c
Jean Fiedler <JFI@kd.local>
2020-07-15 09:33:44 +0200

    Multi Arch Build

--- installer/create installer.cmd
+++ installer/create installer.cmd
@@ -1,4 +1,11 @@
-candle installer.wxs
-light installer.wixobj -dcl:high -ext WixUIExtension.dll -ext WixUtilExtension.dll
+del *.msi
+cargo build --release --target x86_64-pc-windows-msvc
+cargo build --release --target i686-pc-windows-msvc
+candle installer.wxs -arch x64
+light installer.wixobj -dcl:high -ext WixUIExtension.dll -ext WixUtilExtension.dll -out jikwaa-agent-x64.msi
 del installer.wixobj
-del installer.wixpdb
+del *.wixpdb
+candle installer.wxs -arch x86
+light installer.wixobj -dcl:high -ext WixUIExtension.dll -ext WixUtilExtension.dll -out jikwaa-agent-x86.msi
+del installer.wixobj
+del *.wixpdb
